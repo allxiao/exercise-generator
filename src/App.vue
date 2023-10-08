88679ac153d63d398e928de7a5d69bddb52bee58
allxiao <arieshout@gmail.com>
2023-10-08 15:02:34 +0800

    basic form control settings

--- src/App.vue
+++ src/App.vue
@@ -8,29 +8,28 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="#">加法</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+            <a class="nav-link" href="#">减法</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            <a class="nav-link" href="#">乘法</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">除法</a>
           </li>
         </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
+        <form class="d-flex" role="form" id="config">
+          <div class="input-group">
+            <label class="input-group-text" for="input-min-value">最小值</label>
+            <input class="form-control me-2 col-1" id="input-min-value" name="input-min-value" type="number" aria-label="最小值">
+          </div>
+          <div class="input-group">
+            <label class="input-group-text" for="input-max-value">最大值</label>
+            <input class="form-control me-2 col-1" id="input-max-value" name="input-max-value" type="number" aria-label="最大值">
+          </div>
+          <button class="btn btn-outline-success" type="submit">生成</button>
         </form>
       </div>
     </div>
@@ -70,6 +69,14 @@ export default defineComponent({
 #main-page {
   width: 210mm;
   height: 296mm;
+}
+
+#config input {
+  width: 6em;
+}
+
+#config button {
+  width: 8em;
 }
 
 @media print {
