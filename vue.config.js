a356cd80b681eb42964c94301c71d0bac03fa327
allxiao <arieshout@gmail.com>
2023-10-08 15:02:33 +0800

    update page title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+      title: '练习题生成器'
+    }
+  }
 })
